dd92c2e76d6991858c723f2a461ee1a2c8bfbe9e
AngelChuquichambi <102180819+AngelChuquichambi@users.noreply.github.com>
2022-03-28 20:25:19 -0400

    tarea.java

---
+++ Practica2_tareagastar/tarea.java
@@ -0,0 +1,28 @@
+package com.emergentes;
+
+public class tarea {
+
+    private int id;
+    private String tarea;
+
+    public tarea() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTarea() {
+        return tarea;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTarea(String tarea) {
+        this.tarea = tarea;
+    }
+
+}
